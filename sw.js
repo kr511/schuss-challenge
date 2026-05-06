aa142e1abe0bd611526c39376461f812c057cd48
kr511 <eliaskummel@gmail.com>
2026-05-06 16:46:40 +0200

    Remove legacy header profile trigger

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
 //   • Bei Aktivierung werden alte Caches mit anderem Versionsnamen gelöscht.
 // Hinweis: Bei Releases CACHE_VERSION erhöhen UND ?v=X.X in index.html bumpen.
 
-const CACHE_VERSION = 'sc-shell-v9';
+const CACHE_VERSION = 'sc-shell-v10';
 const OFFLINE_URL = './offline.html';
 const PRECACHE_URLS = [
   OFFLINE_URL,
